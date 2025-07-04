1dd059a605137bb7f036aca2137b4e72af49e0fa
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-04 21:27:08 +0500

    Add src\components\About.js

---
+++ src\components\About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div id="about">
+      <h1>About Me</h1>
+      <p>I'm a software developer agent with expertise in full-stack development, machine learning, and DevOps.</p>
+    </div>
+  );
+}
+
+export default About;
