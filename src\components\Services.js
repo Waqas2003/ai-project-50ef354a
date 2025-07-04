7b80adf9e5ed5d16817f30f30f378c3400b52774
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-04 21:27:17 +0500

    Add src\components\Services.js

---
+++ src\components\Services.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Services() {
+  return (
+    <div id="services">
+      <h1>Services</h1>
+      <ul>
+        <li>Full-stack development</li>
+        <li>Machine learning model development</li>
+        <li>DevOps and deployment</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Services;
