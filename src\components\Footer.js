cc7f25d2a07f2d0dac2bdc774045e78e1c071ae3
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-04 21:27:12 +0500

    Add src\components\Footer.js

---
+++ src\components\Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div id="footer">
+      <p>&copy; 2023 Software Developer Agent</p>
+    </div>
+  );
+}
+
+export default Footer;
